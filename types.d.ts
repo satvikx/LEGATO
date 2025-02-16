5b8ef42467560e4a40055d7ad57cb26c9bff26c1
Satvik <satvik.shrivastava.exe@gmail.com>
2025-02-16 22:22:55 +0530

    fixes

--- types.d.ts
+++ types.d.ts
@@ -1,6 +1,7 @@
 import { Connection } from "mongoose"
 
 declare global {
+    // eslint-disable-next-line no-var
     var mongoose : {
         conn: Connection | null;
         promise: Promise<Connection> | null;
